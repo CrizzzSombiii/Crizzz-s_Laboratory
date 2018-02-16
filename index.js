2bf1c3f9e1bff3e4dc8345b5eded4d62352286c0
CrizzzSombiii <cristianstein555@gmail.com>
2018-02-16 23:40:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ res.sendFile(__dirname+"/docs/index.htm")
 app.use("/docs",require("express").static(__dirname+"/public"))
 net.listen(process.env.PORT||3000,()=>{console.log("Ready!")})
 require("socket.io")(net,{}).sockets.on("connection",(socket)=>{
-console.log("socket connection")
+
 })
